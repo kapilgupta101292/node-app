2764b79dfb945be580d5ce828a9950d353e5abaa
kapilgupta101292 <kapilguptansit@gmail.com>
2020-02-09 22:06:45 +0530

    commiting all code

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("<h1>My Node app</h1>");
+});
+app.listen(5000, () => {
+  console.log("App listening on port 5000!");
+});
